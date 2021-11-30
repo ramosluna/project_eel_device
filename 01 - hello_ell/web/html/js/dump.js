b6c9d07007d908f8062343644b071b51ca55a5f0
ramosluna <claudioramosluna@hotmail.com>
2021-11-30 08:21:11 -0300

    finish_id002

--- 01 - hello_ell/web/html/js/dump.js
+++ 01 - hello_ell/web/html/js/dump.js
@@ -1,4 +1,4 @@
-
+//#region js_showAnaliseEprom363
 eel.expose(js_showAnaliseEprom363);
 function js_showAnaliseEprom363(){
     var target = document.getElementById('relatorio')
@@ -112,8 +112,8 @@ for ( var x = 480; x < 491; x++){
         getHexPlanilha='';
 
   //Dataset SET (10A0) DataSet_number 8
-  let getHexData = '';
-  let getHexIndex = '';
+  var getHexData = '';
+  var getHexIndex = '';
         for ( var x = 4256; x < 4264; x++){
             getHexData +=  document.getElementById(x).innerHTML
         }
@@ -143,40 +143,122 @@ for ( var x = 480; x < 491; x++){
         let convert5 = convertHexToASCII(fatiar3)
         document.getElementById("DataSet_Asci_index").value = convert5
 
-//Dataset BDS (0x164)
-    let getBdsData = '';
-    let getBDSIndex = '';
-        for ( var x = 4246; x < 4564; x++){
-            getBdsData +=  document.getElementById(x).innerHTML
+//Dataset KDS (0x164)
+
+    var getKdsData = '';
+    var getKDSIndex = '';
+        for ( var x = 356; x < 364; x++){
+            getKdsData +=  document.getElementById(x).innerHTML
         }
-        for ( var x = 4564; x < 4566; x++){
-            getBDSIndex +=  document.getElementById(x).innerHTML
+        for ( var x = 364; x < 366; x++){
+            getKDSIndex +=  document.getElementById(x).innerHTML
         }
-        document.getElementById("BDS_number").value = getBDSData;
-        document.getElementById("BDS_index_number").value = getBDSIndex;
+        document.getElementById("kds_number").value = getKdsData;
+        document.getElementById("kds_index").value = getKDSIndex;
 
         var fatiar3 = '';
         var end3 = 2;
-         for(x=0;x<getBDSData.length;x+=2){
-            fatiar3 += getBDSData.slice(x,end3);
+         for(x=0;x<getKdsData.length;x+=2){
+            fatiar3 += getKdsData.slice(x,end3);
             fatiar3 += ' ';
             end3 += 2;
         }     
         let convert6 = convertHexToASCII(fatiar3)
-        document.getElementById("BDS_Asci_number").value = convert6
+        document.getElementById("kds_asci_number").value = convert6
 
         var fatiar3 = '';
         var end3 = 2;
-         for(x=0;x<getBDSIndex.length;x+=2){
-            fatiar3 += getBDSIndex.slice(x,end3);
+         for(x=0;x<getKDSIndex.length;x+=2){
+            fatiar3 += getKDSIndex.slice(x,end3);
             fatiar3 += ' ';
             end3 += 2;
         }
         let convert7 = convertHexToASCII(fatiar3)
-        document.getElementById("BDS_Asci_index").value = convert7
+        document.getElementById("kds_asci_index").value = convert7
 
+        //Dataset BDS (0x164)
+    var getBdsData = '';
+    var getBdsindex = '';
+        for ( var x = 4246; x < 4254; x++){
+            getBdsData +=  document.getElementById(x).innerHTML
+        }
+        for ( var x =4254; x < 4256; x++){
+            getBdsindex +=  document.getElementById(x).innerHTML
+        }
+        document.getElementById("bds_number").value = getBdsData;
+        document.getElementById("bds_index").value = getBdsindex;
+
+        var fatiar3 = '';
+        var end3 = 2;
+         for(x=0;x<getBdsData.length;x+=2){
+            fatiar3 += getBdsData.slice(x,end3);
+            fatiar3 += ' ';
+            end3 += 2;
+        }     
+        let convert8 = convertHexToASCII(fatiar3)
+        document.getElementById("bds_asci_number").value = convert8
+
+        var fatiar3 = '';
+        var end3 = 2;
+         for(x=0;x<getBdsindex.length;x+=2){
+            fatiar3 += getBdsindex.slice(x,end3);
+            fatiar3 += ' ';
+            end3 += 2;
+        }
+        let convert9 = convertHexToASCII(fatiar3)
+        document.getElementById("bds_asci_index").value = convert9
+
+    //EngSpeedY()
+        var engY = '';
+        var arrayEngY =[];
+        var getTwoBytes = 1;
+        var addresArrayEngY = 0;
+        let valueHexEngY=['engY_hex_0','engY_hex_1','engY_hex_2','engY_hex_3']
+        let valueDecEngY=['engY_dec_0','engY_dec_1','engY_dec_2','engY_dec_3']
+        for (x=1063;x<1071;x++){
+
+            arrayEngY.unshift(document.getElementById(x).innerHTML);    
+            if (getTwoBytes == 2){ // SÓ ENTRA NESSE IF SE ESTIVER 2 BYTES NO ARRAY
+                engY += arrayEngY[0];
+                engY += arrayEngY[1];
+                document.getElementById(valueHexEngY[addresArrayEngY]).value = engY;
+                engY = parseInt(engY,16)
+                document.getElementById(valueDecEngY[addresArrayEngY]).value = engY;
+                arrayEngY = []
+                getTwoBytes = 0;
+                addresArrayEngY+=1
+                engY = '';
+            }
+            getTwoBytes++; 
+        }    
+        //VehicleSpeedY()
+        var espY = '';
+        var arrayEspY =[];
+        var getTwoBytes = 1;
+        var addresArrayEspY = 0;
+        let valueHexEspY=['speedY_hex_0','speedY_hex_1','speedY_hex_2','speedY_hex_3','speedY_hex_4']
+        let valueDecEspY=['speedY_dec_0','speedY_dec_1','speedY_dec_2','speedY_dec_3','speedY_dec_4']
+        for (x=1028;x<1038;x++){
+
+            arrayEspY.unshift(document.getElementById(x).innerHTML);    
+            if (getTwoBytes == 2){ // SÓ ENTRA NESSE IF SE ESTIVER 2 BYTES NO ARRAY
+                espY += arrayEspY[0];
+                espY += arrayEspY[1];
+                document.getElementById(valueHexEspY[addresArrayEspY]).value = espY;
+                espY = parseInt(espY,16)
+                document.getElementById(valueDecEspY[addresArrayEspY]).value = espY;
+                arrayEspY = []
+                getTwoBytes = 0;
+                addresArrayEspY+=1
+                espY = '';
+            }
+            getTwoBytes++;           
+
+        }      
 }
+//#endregion
 
+//#region Function convert Hex to Ascii
 function convertHexToASCII(hexString){
     let stringOut = '';
     hexString.split(' ').map((i) => {
@@ -185,21 +267,29 @@ function convertHexToASCII(hexString){
     });
     return stringOut.slice(0,-1)
 }
+//#endregion
 
+//#region function button html dataset
 function js_dSet(){
  eel.dataSet()
  }
+//#endregion
 
+//#region LoadDump
 function loadDump(){
     limpar()
     eel.dumpHtml()
 }
+//#endregion
 
+//#region canoe
 function canoe(){
     document.getElementById("bt").innerHTML = "VC CLICKOU !"
     eel.automatico()
 }
+//#endregion
 
+//#region limpar
 function limpar(){
     console.log("caled clear")
     for(var i=0; i<4112;i++){
@@ -211,17 +301,24 @@ function limpar(){
     document.getElementById('dSet').innerHTML = 'path DatSet:'
      }
 }
+//#endregion
 
+//#region js_setPathDump
 eel.expose(js_setPathDump);
 function js_setPathDump(paths){
      document.getElementById('path').innerHTML = paths
     }
+//#endregion
 
+//#region js_setPathDataSet
 eel.expose(js_setPathDataSet);
 function js_setPathDataSet(paths){
      document.getElementById('dSet').innerHTML = paths
     }
 
+//#endregion
+
+//#region js_imprimirHex
 eel.expose(js_imprimirHex);
 function js_imprimirHex(address, valoresdados) {
 	var inicio = 0;
@@ -233,14 +330,16 @@ function js_imprimirHex(address, valoresdados) {
 		   end += 2;
         }
    }
+//#endregion
 
+//#region js_imprimirHexSeForDiferente
 eel.expose(js_imprimirHexSeForDiferente);
 function js_imprimirHexSeForDiferente(address, valoresdados){
 /*compara com o que já esta na tela e imprime se estiver igual*/
 
   var inicio = 0;
-	var end = 2;
-	var getdump = 0;
+  var end = 2;
+  var getdump = 0;
   var getDaSet = 0;
 
         for(var x=0;x<16;x++){
@@ -268,4 +367,4 @@ function js_imprimirHexSeForDiferente(address, valoresdados){
         }
 }
 
-
+ //#endregion
